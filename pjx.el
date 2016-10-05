ab85490e0a6df9f79286bc41b5a20474b7641369
CaioRS <caiorss.rodrigues@gmail.com>
2016-10-04 23:14:28 -0300

    Changed internal functions name to Emacs convention.

--- pjx.el
+++ pjx.el
@@ -40,18 +40,18 @@
 (defvar pjx/current-project nil "Holds the current project name")
 
 
-(defun pjx/with-directory (directory fn)
+(defun pjx--with-directory (directory fn)
   "Runs a function fn inside a directory setting default-directory."
   (let ((default-directory (file-name-as-directory directory)))
     (funcall fn)))
 
 
-(defun pjx/file-in-directory-p (directory filename)
+(defun pjx--file-in-directory-p (directory filename)
   (equal (file-name-as-directory (expand-file-name (file-name-directory filename)))
          (file-name-as-directory (expand-file-name directory))))
 
 
-(defun pjx/directory-files (directory)
+(defun pjx--directory-files (directory)
   "List a directory not showing files starting with (.), (~), (#) "
   (remove-if-not
    
@@ -73,20 +73,20 @@
 
    (directory-files directory t)))
 
-(defun pjx/project-list ()
+(defun pjx--project-list ()
   "Returns all projects directories. See full doc."
   (mapcar (lambda (p) (cons (file-name-nondirectory p) p))
-          (pjx/directory-files pjx/project-root)))
+          (pjx--directory-files pjx/project-root)))
 
 
-(defun pjx/project-get-files ()
+(defun pjx--project-get-files ()
   "Returns all files in project directory"
   (mapcar (lambda (p) (cons (file-name-nondirectory p) p))          
-          (pjx/directory-files pjx/current-project))
+          (pjx--directory-files pjx/current-project))
   )
 
 
-(defun pjx/project-get-git-files ()
+(defun pjx--project-get-git-files ()
   "Returns a list of files tracked by git in project master branch."
   (let* (
          (default-directory (file-name-as-directory pjx/current-project))
@@ -96,45 +96,45 @@
     (split-string (replace-regexp-in-string "\n+$" "" outp) "\n")))
 
 
-(defun pjx/project-get-buffers ()
+(defun pjx--project-get-buffers ()
   "Get all buffers related to current project."
   (mapcar (lambda (b) (cons (buffer-name b) b))
           
           (remove-if-not (lambda (b)
                            (and (buffer-file-name b)
-                                (pjx/file-in-directory-p  pjx/current-project
+                                (pjx--file-in-directory-p  pjx/current-project
                                                           (buffer-file-name b))))
 
                          (buffer-list)
                    )))
 
-(defun pjx/project-helm-fn (callback)
+(defun pjx--project-helm-fn (callback)
   ""
   (helm
    :prompt "Project: "
    :sources  `((
                 (name       . "Pjx: ")
-                (candidates . ,(pjx/project-list))
+                (candidates . ,(pjx--project-list))
                 (action     . callback)
                 ))))
 
 
-(defun pjx/project-files-helm-fn (callback)
+(defun pjx--project-files-helm-fn (callback)
   (helm
    :prompt "Project File: " 
    :sources  `((
                 (name       . "Dir: ")
-                (candidates . ,(pjx/project-get-files))
+                (candidates . ,(pjx--project-get-files))
                 (action     . callback)
                 ))))
 
 
-(defun pjx/project-buffers-helm-fn (callback)
+(defun pjx--project-buffers-helm-fn (callback)
   (helm
    :prompt "Project File: " 
    :sources  `((
                 (name       . "Dir: ")
-                (candidates . ,(pjx/project-get-buffers))
+                (candidates . ,(pjx--project-get-buffers))
                 (action     . callback)
                 ))))
 
@@ -154,9 +154,25 @@
   (command-apropos "pjx/"))
 
 (defun pjx/helm ()
-  "Select project directory and open it in Helm."
+  "Select project directory and open it in dired-mode."
   (interactive)
-  (pjx/project-helm-fn (lambda (p)                         
+  (pjx--project-helm-fn (lambda (p)                         
+                         (setq pjx/current-project p)
+                         ;;(dired p)
+
+                         (if pjx/current-project
+                             (progn  (pjx/close)
+                                     (setq pjx))
+
+                           (setq pjx/current-project p)
+                           (dired p)
+                           ))))
+
+
+(defun pjx/helm-select ()
+  "Select project directory."
+  (interactive)
+  (pjx--project-helm-fn (lambda (p)                         
                          (setq pjx/current-project p)
                          ;;(dired p)
 
@@ -167,7 +183,6 @@
                            (setq pjx/current-project p)
                            )
                          )))
-
 
 (defun pjx/dir ()
   "Open current project set with pjx/helm.
@@ -181,20 +196,20 @@
 (defun pjx/file-open  ()
   "Select a file of current project and open it."
   (interactive)
-  (pjx/project-files-helm-fn #'find-file))
+  (pjx--project-files-helm-fn #'find-file))
 
 (defun pjx/file-git-open-all ()
   "Open all files listed in git current branch."
   (interactive)
   (let ((default-directory  (file-name-as-directory pjx/current-project)))
-    (mapc #'find-file-noselect (pjx/project-get-git-files))
+    (mapc #'find-file-noselect (pjx--project-get-git-files))
     (dired pjx/current-project)
     ))
 
 (defun pjx/file-switch ()
   "Switch between project files."
   (interactive)
-  (pjx/project-buffers-helm-fn #'switch-to-buffer))
+  (pjx--project-buffers-helm-fn #'switch-to-buffer))
 
 
 (defun pjx/panel ()
@@ -216,7 +231,7 @@
                (file-path (buffer-file-name b))
                )
             
-            (if (and file-path (pjx/file-in-directory-p pjx/current-project file-path))
+            (if (and file-path (pjx--file-in-directory-p pjx/current-project file-path))
 
                 (with-current-buffer b
                   (save-buffer)
@@ -227,9 +242,8 @@
 
         (buffer-list))
 
-  (message "Project files closed")
-  
-  ) ;; End of pxj/close
+  (message "Project files closed"))
+   ;; End of pxj/close
 
 
 (defun pjx/dired-move ()
@@ -256,7 +270,7 @@
 
   (interactive)
   
-  (pjx/with-directory pjx/current-project
+  (pjx--with-directory pjx/current-project
                       (lambda ()
                         (start-process "term.proj" ;; Process name 
                                        nil ;; buffer name
@@ -266,7 +280,7 @@
 (defun pjx/project-new ()
   "Creates a new project."
   (interactive)
-  (pjx/with-directory pjx/project-root
+  (pjx--with-directory pjx/project-root
 
                       (lambda ()
                         (let
